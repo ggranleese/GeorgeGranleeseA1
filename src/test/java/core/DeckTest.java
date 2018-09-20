54847e5b930860dab65054345d7690814ef9840a
ggranleese <georgegranleese@Georges-MacBook-Pro-2.local>
2018-09-19 23:54:28 -0400

    Created DeckTest file

---
+++ src/test/java/core/DeckTest.java
@@ -0,0 +1,49 @@
+package core;
+
+import junit.framework.TestCase;
+
+public class DeckTest extends TestCase{
+	
+	public void testPopulateDeck(){
+		Deck deck = new Deck();
+		deck.populate();
+		assertEquals(52, deck.getCardsRemaining());
+	}
+	
+	public void testAddCard() {
+		Deck deck = new Deck();
+		Card card = new Card(1,1);
+		deck.add(card);
+		assertEquals(1, deck.getCardsRemaning());
+	}
+	
+	public void testRemoveCard() {
+		Deck deck = new Deck();
+		Card card = new Card(1,1);
+		deck.add(card);
+		deck.remove(card);
+		assertEquals(0, deck.getCardsRemaining());
+	}
+	
+	public void testDealCard() {
+		Deck deck = new Deck();
+		deck.populate();
+		Card card = new Card(1,1);
+		assertEquals(card, deck.dealCard());
+	}
+	
+	public void testGetCard() {
+		Deck deck = new Deck();
+		deck.populate();
+		assertEquals("HA", deck.get(0));
+	}
+	
+	public void testToString() {
+		Deck deck = new Deck();
+		Card card = new Card(1,1);
+		deck.add(card);
+		assertEquals("HA", deck.toString());
+	}
+
+	
+}
