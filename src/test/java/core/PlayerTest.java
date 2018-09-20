60c756ef65865485823aa7509ccca7810555ee14
ggranleese <georgegranleese@Georges-MacBook-Pro-2.local>
2018-09-20 02:14:53 -0400

    Initially forgot players repeated hitting test

--- src/test/java/core/PlayerTest.java
+++ src/test/java/core/PlayerTest.java
@@ -43,6 +43,24 @@ public class PlayerTest extends TestCase {
 		assertTrue(player.getScore() > scoreCheck);
 	}
 	
+	public void testHitRepeat() {
+		Player player = new Player(new Deck());
+		player.newRound();
+		Deck deck = new Deck();
+		deck.add(new Card(2,5));
+		deck.add(new Card(2,3));
+		Card card1 = new Card(1,5);
+		Card card2 = new Card(1,4);
+		player.getPlayerDeck().add(card1);
+		player.getPlayerDeck().add(card2);
+		int scoreCheck1 = player.getScore();
+		player.hit(deck);
+		int scoreCheck2 = player.getScore();
+		player.hit(deck);
+		int scoreCheck3 = player.getScore();
+		assertTrue(scoreCheck1 < scoreCheck2 && scoreCheck2 < scoreCheck3);
+	}
+	
 	public void testStand(){
 		Player player = new Player(new Deck());
 		player.newRound();
